e560d23ba6bb69368dc62ea6daf92e1384377e18
Adrian Guerrera <adrianguerrera@gmail.com>
2018-09-02 20:56:31 +1000

    Added audit files

---
+++ audit/test/BokkyPooBahsDateTimeLibrary.js
@@ -0,0 +1,11 @@
+var dateTimeLibOutput={
+  "contracts" : 
+  {
+    "BokkyPooBahsDateTimeLibrary.sol:BokkyPooBahsDateTimeLibrary" : 
+    {
+      "abi" : "[]",
+      "bin" : "604c602c600b82828239805160001a60731460008114601c57601e565bfe5b5030600052607381538281f30073000000000000000000000000000000000000000030146080604052600080fd00a165627a7a72305820a08d0ee62d378eeea2f6ab903a364f4a61090fcc7a6ea88f03c87931c1197fc00029"
+    }
+  },
+  "version" : "0.4.23+commit.124ca40d.Darwin.appleclang"
+};
